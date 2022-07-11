aa35c8a8f4875b34755dbffe554282d01ee2b087
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-07-11 21:22:51 +0200

    Second commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import AvailableMeals from "./AvailalbleMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
